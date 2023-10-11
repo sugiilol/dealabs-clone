35910077c5a4f5566d1e75d28d0da0210f6fd088
sugiilol <sugii.noob@gmail.com>
2023-10-11 18:35:38 +0200

    started the userCard for dealCard

---
+++ dealabs-clone/src/Components/userCard/userCard.jsx
@@ -0,0 +1,10 @@
+import "./userCard.css"
+
+export default function userCard(){
+
+    return(
+        <>
+            <h2>Salut</h2>
+        </>
+    )
+}
---
+++ dealabs-clone/src/Data/UsersData.jsx
@@ -0,0 +1,12 @@
+import avatarUser1 from "./img/avatarUser1.jpg"
+
+export const users = [
+    {
+        pseudo: "titi28043",
+        yearInscription : "2021",
+        avatar : avatarUser1,
+        nbOfPost: 973,
+        idPosts : [1, 2 , 3],
+        nbOfLike: 11050
+    }
+]
