82704f266ca28c5f34c5f125b8b54f6b31bc3684
SuGii <sugii.noob@gmail.com>
2023-10-17 07:05:32 +0200

    userCard updated

--- dealabs-clone/src/Components/userCard/userCard.jsx
+++ dealabs-clone/src/Components/userCard/userCard.jsx
@@ -1,10 +1,23 @@
 import "./UserCard.css"
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import SellOutlinedIcon from '@mui/icons-material/SellOutlined';
 
-export default function UserCard(props){
+export default function UserCard(props) {
 
-    return(
-        <div>
-            <h2>{props.user.pseudo}</h2>
+    return (
+        <div className="container-usercard">
+            <div className="usercard-avatar">
+                <img src={props.user.avatar} alt="user-avatar" />
+            </div>
+            <div className="usercard-user-infos">
+                <div>
+                    Partagé par <span>{props.user.pseudo}</span>
+                </div>
+                <div>
+                    <CalendarMonthIcon className="icon-custom" />Membre depuis {props.user.yearInscription} <SellOutlinedIcon className="icon-custom" /> {props.user.nbOfPost} <ThumbUpOffAltIcon className="icon-custom" />{props.user.nbOfLike}
+                </div>
+            </div>
         </div>
     )
 }
--- dealabs-clone/src/Data/UsersData.jsx
+++ dealabs-clone/src/Data/UsersData.jsx
@@ -1,13 +1,13 @@
-// import avatarUser1 from "./img/avatarUser1.jpg"
+import avatarUser1 from "./img/avatarUser1.jpg"
 
 export const users = [
     {
         id:1,
         pseudo: "titi28043",
         yearInscription : "2021",
-        // avatar : avatarUser1,
-        nbOfPost: 973,
+        avatar : avatarUser1,
+        nbOfPost: 1,
         idPosts : [1, 2 , 3],
-        nbOfLike: 11050
+        nbOfLike: 2
     }
 ]
